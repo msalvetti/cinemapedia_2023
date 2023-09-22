44c7fac89d27137cccb1ca721e10d92dd5c3d21e
Mariano Salvetti <mariano.salvetti@globant.com>
2023-09-22 17:00:54 -0300

    agregando el card swiper y los detalles de la UI.

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia_2023/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia_2023/presentation/widgets/movies/movies_slideshow.dart';
 import 'package:cinemapedia_2023/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -36,17 +37,7 @@ class _HomeViewState extends ConsumerState<_HomeView> {
     return Column(
       children: [
         CustomAppbar(),
-        Expanded(
-          child: ListView.builder(
-            itemCount: nowPlayingMovies.length,
-            itemBuilder: (context, index) {
-              final movie = nowPlayingMovies[index];
-              return ListTile(
-                title: Text(movie.title),
-              );
-            },
-          ),
-        ),
+        MoviesSlideshow(movies: nowPlayingMovies),
       ],
     );
   }
---
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -0,0 +1,13 @@
+import 'package:cinemapedia_2023/domain/entities/movie.dart';
+import 'package:flutter/material.dart';
+
+class MoviesSlideshow extends StatelessWidget {
+  final List<Movie> movies;
+
+  const MoviesSlideshow({super.key, required this.movies});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
