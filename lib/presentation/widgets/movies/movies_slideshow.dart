65953b2afa4127aa4521c6ffbe7bc30e30e37704
Mariano Salvetti <mariano.salvetti@globant.com>
2023-09-22 17:32:05 -0300

    agregando el card swiper y los detalles de la UI.

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia_2023/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +9,28 @@ class MoviesSlideshow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      height: 210,
+      width: double.infinity,
+      child: Swiper(
+        itemBuilder: (context, index) {
+          final movie = movies[index];
+          return _Slide(movie: movie);
+        },
+        itemCount: movies.length,
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+
+  const _Slide({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Placeholder();
   }
 }
