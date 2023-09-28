bf4e283b0a3cbd7ca52b9048e872c47078e6c59c
Mariano Salvetti <mariano.salvetti@globant.com>
2023-09-28 17:31:51 -0300

    navegacion al detalle de la pelicula con go router funcionando.

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -2,6 +2,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia_2023/config/helpers/human_formats.dart';
 import 'package:cinemapedia_2023/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MovieHorizontalListview extends StatefulWidget {
   final List<Movie> movies;
@@ -92,16 +93,16 @@ class _Slide extends StatelessWidget {
                   width: 150,
                   loadingBuilder: (context, child, loadingProgress) {
                     if (loadingProgress != null) {
-                      return Padding(
+                      return const Padding(
                         padding: EdgeInsets.all(8.0),
-                        child: const Center(
-                          child: CircularProgressIndicator(
-                            strokeWidth: 2,
-                          ),
-                        ),
+                        child: Center(
+                            child: CircularProgressIndicator(strokeWidth: 2)),
                       );
                     }
-                    return FadeIn(child: child);
+                    return GestureDetector(
+                      onTap: () => context.push('/movie/${movie.id}'),
+                      child: FadeIn(child: child),
+                    );
                   },
                 ),
               ),
