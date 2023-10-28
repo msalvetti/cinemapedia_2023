35e8fca1ad4622aefe9e3b1030b42fdcf676e2c2
Mariano Salvetti <mariano_salvetti@gap.com>
2023-10-28 16:21:20 -0300

    navegacion entre pantallas estable. Varias librerias actualizadas.

---
+++ lib/presentation/views/movies/home_view.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../providers/providers.dart';
+import '../../widgets/movies/movie_horizontal_listview.dart';
+import '../../widgets/movies/movies_slideshow.dart';
+import '../../widgets/shared/custom_appbar.dart';
+import '../../widgets/shared/full_screen_loader.dart';
+
+class HomeView extends ConsumerStatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  HomeViewState createState() => HomeViewState();
+}
+
+class HomeViewState extends ConsumerState<HomeView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+    ref.read(popularMoviesProvider.notifier).loadNextPage();
+    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
+    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final initialLoading = ref.watch(initialLoadingProvider);
+
+    if (initialLoading) {
+      return const FullScreenLoader();
+    }
+
+    final slideshowMovies = ref.watch(movieSlideshowProvider);
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final popularMovies = ref.watch(popularMoviesProvider);
+    final upcomingMovies = ref.watch(upcomingMoviesProvider);
+    final topRatedMovies = ref.watch(topRatedMoviesProvider);
+
+    return CustomScrollView(
+      slivers: [
+        const SliverAppBar(
+          floating: true,
+          flexibleSpace: FlexibleSpaceBar(
+            centerTitle: true,
+            title: CustomAppbar(),
+          ),
+        ),
+        SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+          return Column(children: [
+            MoviesSlideshow(movies: slideshowMovies),
+            MovieHorizontalListview(
+              movies: nowPlayingMovies,
+              title: 'En cines',
+              subTitle: 'Lunes 20',
+              loadNextPage: () {
+                ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+              },
+            ),
+            MovieHorizontalListview(
+              //upcoming
+              movies: upcomingMovies,
+              title: 'Proximamente',
+              subTitle: 'en este mes',
+              loadNextPage: () {
+                ref.read(upcomingMoviesProvider.notifier).loadNextPage();
+              },
+            ),
+            MovieHorizontalListview(
+              movies: popularMovies,
+              title: 'Populares',
+              // subTitle: 'en este mes',
+              loadNextPage: () {
+                ref.read(popularMoviesProvider.notifier).loadNextPage();
+              },
+            ),
+            MovieHorizontalListview(
+              //top_rated
+              movies: topRatedMovies,
+              title: 'Mejores calificadas',
+              subTitle: 'desde siempre',
+              loadNextPage: () {
+                ref.read(topRatedMoviesProvider.notifier).loadNextPage();
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            )
+          ]);
+        }, childCount: 1)),
+      ],
+    );
+  }
+}
--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  const CustomBottomNavigation({Key? key}) : super(key: key);
+  final int currentIndex;
+
+  CustomBottomNavigation({super.key, required this.currentIndex});
+
+  //using context.go() I need to use the index and define which screen to go
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/home/0');
+        break;
+      case 1:
+        context.go('/home/1');
+        break;
+      case 2:
+        context.go('/home/2');
+        break;
+      default:
+        context.go('/home/0');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(elevation: 0, items: const [
-      BottomNavigationBarItem(icon: Icon(Icons.home_max), label: "Inicio"),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.label_outline), label: "Categorias"),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.favorite_outline), label: "Favoritos"),
-    ]);
+    return BottomNavigationBar(
+        elevation: 0,
+        currentIndex: currentIndex,
+        onTap: (index) => onItemTapped(context, index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_max), label: "Inicio"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.label_outline), label: "Categorias"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline), label: "Favoritos"),
+        ]);
   }
 }
